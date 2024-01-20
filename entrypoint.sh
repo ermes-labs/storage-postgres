1279a38638b1f12601167f8647047fb7a2d77fd0
Paolo <paololongo.git@gmail.com>
2024-01-20 18:01:06 +0100

    Rename setup template

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # - ERMES_NODE_ID: the node ID of the ERMES node to be created
 
 # Generate a new SQL file from the template, replacing the placeholder
-sed "s/ERMES_NODE_ID/${ERMES_NODE_ID}/g" /docker-entrypoint-initdb.d/00_setup.template.sql >/docker-entrypoint-initdb.d/00_setup.sql
+sed "s/ERMES_NODE_ID/${ERMES_NODE_ID}/g" /docker-entrypoint-initdb.d/00_setup.sql.template >/docker-entrypoint-initdb.d/00_setup.sql
 
 # Call the original entrypoint script
 exec docker-entrypoint.sh postgres
